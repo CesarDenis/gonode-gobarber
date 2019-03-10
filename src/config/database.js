6c33add81292d1f33408079f417c1ddf6f09dd31
Cesar Denis <cesar@cesardenis.com>
2019-03-10 20:29:57 -0300

    chore: config sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'gonode_gobarber',
+  defile: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
