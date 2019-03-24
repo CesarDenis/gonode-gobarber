45f950aaa42fcec5684f54b5fd3bbef277b2a6cb
Cesar Denis <cesar@cesardenis.com>
2019-03-24 14:02:19 -0300

    feature: add providers list in dashboard

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,15 @@ const authMiddleware = require('./app/middlewares/auth')
 const flashMiddleware = require('./app/middlewares/flash')
 const guestMiddleware = require('./app/middlewares/guest')
 
+const FilesController = require('./app/controllers/FilesController')
 const SessionController = require('./app/controllers/SessionController')
 const UserController = require('./app/controllers/UserController')
 
+const DashboardController = require('./app/controllers/DashboardController')
+
 routes.use(flashMiddleware)
+
+routes.get('/files/:file', FilesController.show)
 
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
@@ -24,9 +29,6 @@ routes.use('/app', authMiddleware)
 
 routes.get('/app/logout', SessionController.destroy)
 
-routes.get('/app/dashboard', (req, res) => {
-  console.log(req.session.user)
-  return res.render('dashboard')
-})
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
